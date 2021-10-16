74c30fa0b8aa0f714315c271d6e7bb59c7a1a1dc
Box In A Box <evanferrao@gmail.com>
2021-10-16 19:27:28 +0530

    ANXCamera: Initial ANXCamera v204

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -4,6 +4,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64)
 
 PRODUCT_PACKAGES += \
